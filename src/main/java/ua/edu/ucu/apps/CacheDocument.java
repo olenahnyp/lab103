79a54b528e7fe674731f8df152cfef9ae4afe46a
Olena <gnypolenka@gmail.com>
2023-11-27 18:58:22 +0200

    add database

--- src/main/java/ua/edu/ucu/apps/CacheDocument.java
+++ src/main/java/ua/edu/ucu/apps/CacheDocument.java
@@ -7,46 +7,68 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CacheDocument {
+    private Document doc;
 
+    public CacheDocument(Document document) {
+        this.doc = document;
+    }
     private Connection connect() {
-        String url = "jdbc:sqlite:sqlite.db";
+        String url = "jdbc:sqlite:sqlite2.db";
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            handleSQLException(e);
         }
         return conn;
     }
+    private void handleSQLException(SQLException e) {
+        System.err.println("SQL Exception: " + e.getMessage());
+    }
+    public void createTable() {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS documents ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "path TEXT NOT NULL,"
+                + "document TEXT NOT NULL);";
+
+        try (Connection conn = this.connect();
+             PreparedStatement preparedStatement = conn.prepareStatement(createTableSQL)) {
+            preparedStatement.executeUpdate();
+            System.out.println("Database created");
+        } catch (SQLException e) {
+            handleSQLException(e);
+        }
+    }
     public boolean recordExists(String gcsPath) {
-        String sql = "SELECT count(*) FROM your-table WHERE gcs_path = ?";
+        String sql = "SELECT count(*) FROM documents WHERE path = ?";
         try (Connection conn = this.connect();
             PreparedStatement pstmt = connect().prepareStatement(sql)) {
             pstmt.setString(1, gcsPath);
             ResultSet rs = pstmt.executeQuery();
             return rs.getInt(1) > 0;
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            handleSQLException(e);
         }
         return false;
     }
     public void injectText(String text, String gcsPath) {
-        String sql = "INSERT INTO your_table (gcs_path, yourcolumn) VALUES(?, ?)";
+        String sql = "INSERT INTO documents (path, document) VALUES(?, ?)";
         try (Connection conn = this.connect();
             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, gcsPath);
             pstmt.setString(2, text);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            handleSQLException(e);
         }
     }
-
     public static void main(String[] args) {
-        CacheDocument app = new CacheDocument();
         String gcsPath = "gs://cv-examples/wiki.png";
+        CacheDocument app = new CacheDocument(new SmartDocument(gcsPath));
+        app.connect();
+        app.createTable();
         if (app.recordExists(gcsPath)) {
-            app.injectText("Text", gcsPath);
+            app.injectText("text1", gcsPath);
             System.out.println("Record updated successfully");
         }
         else {
